4f43004cc542d858da5732d56cf25bd88945e9dd
Victor Williams <vrwilliams719@gmail.com>
2022-08-08 22:53:36 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import {Box, Container, Link} from "@chakra-ui/react";
 const Footer = () => {
   return (
     <Box py={4} align="center" opacity={0.8} fontSize="2xl">
-      Made by{" "}
+      Contribute to{" "}
       <Link color="teal.500" href="https://github.com/Utkarshn10/Focusly">
-        Utkarsh Nagar
+        Focusly
       </Link>
     </Box>
   );
