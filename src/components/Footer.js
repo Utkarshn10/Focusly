ce0e1af78dacc83bcf0d16910dec2001c655229e
aadil494 <8232aadil@gmail.com>
2022-08-21 12:24:11 +0530

    fixed footer background color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import {Box, Container, Link} from "@chakra-ui/react";
 
 const Footer = () => {
   return (
-    <Box py={4} align="center" opacity={0.8} fontSize="2xl">
+    <Box py={4} align="center" opacity={0.8} fontSize="2xl" background={"#edf2f7"}>
       Contribute to{" "}
       <Link color="teal.500" href="https://github.com/Utkarshn10/Focusly">
         Focusly
