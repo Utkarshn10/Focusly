9e43404ba0b2794b6904b28f379fb1fbf6adbc21
Caleb <caleb.cyrill@gmail.com>
2022-08-21 01:30:39 -0500

    Added the same container from the header to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,15 @@
-import {Box, Container, Link} from "@chakra-ui/react";
+import { Box, Container, Link } from "@chakra-ui/react";
 
 const Footer = () => {
   return (
-    <Box py={4} align="center" opacity={0.8} fontSize="2xl">
-      Contribute to{" "}
-      <Link color="teal.500" href="https://github.com/Utkarshn10/Focusly">
-        Focusly
-      </Link>
-    </Box>
+    <Container maxW={"100%"} bg={"#f7f7f7"}>
+      <Box py={4} align="center" opacity={0.8} fontSize="2xl">
+        Contribute to{" "}
+        <Link color="teal.500" href="https://github.com/Utkarshn10/Focusly">
+          Focusly
+        </Link>
+      </Box>
+    </Container>
   );
 };
 
