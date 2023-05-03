8d38d89035569ee7fd0b7910b0bcecea6f1867f3
GiriMadhan <girimadhan7@gmail.com>
2023-05-03 14:55:18 +0530

    fix: reduced the border radius of tabs button.

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,26 +1,18 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { Tabs, Tab, TabList, Box, Center } from "@chakra-ui/react";
 
-import {
-  Tabs,
-  Tab,
-  TabList,
-  Box,
-  Center,
-
-} from "@chakra-ui/react";
 function TabPanel() {
   return (
     <Center>
       <Box py={4}>
-        <Tabs variant="soft-rounded" colorScheme="teal">
+        <Tabs className="tabs" variant="soft-rounded" colorScheme="teal">
           <TabList>
             <Tab>
               <NavLink to="/">Work</NavLink>
             </Tab>
             <Tab>
               <NavLink to="/Lofi">Lo-fi</NavLink>
-
             </Tab>
             <Tab>
               <NavLink to="/Meditate">Meditate</NavLink>
