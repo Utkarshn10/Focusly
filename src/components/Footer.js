2794e4f478b01f2d3932185465d209ba00ecfcb5
Anubhav <aadhikari1248@gmail.com>
2023-08-08 11:11:14 +0530

    Fixed Footer in mobile view #96

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,23 +14,25 @@ const Footer = () => {
   }
 
   return (
-    <Container maxW={"100%"} bg={"#f7f7f7"}  borderBottomWidth='6px' borderColor='teal'>
-      <Grid templateColumns="1fr auto" gap={4} alignItems="center"  p={4}>
-        <Box opacity={0.8} fontSize="2xl" justifySelf="middle"  ms={{base: 0 , md: "150px" , lg: "150px"}}>
-          🎯 Built by Team{" "}
-          <Link color="teal.500" href="https://github.com/Utkarshn10/Focusly">
-            Focusly
-          </Link>
-        </Box>
-        <PopupButton
-          id="pmedcZ7N"
-          style={buttonStyle}
-          size={66}
-        >
-          <span style={{ marginLeft: 10 }}>🌱 Give Your Valuable Feedback</span>
-        </PopupButton>
-        {/* <Link justifySelf="end" borderWidth='2px' borderColor='teal' py={2} px={2} borderRadius='6px' href="https://github.com/sponsors/Utkarshn10">Support Focusly</Link> */}
-      </Grid>
+    <Container maxW={"100%"} bg={"#f7f7f7"}  borderBottomWidth='6px' borderColor='teal' >
+      <Grid templateColumns={{ base: "1fr", md: "1fr auto" }} gap={4} alignItems="center" className="wrap" p={4}>
+  <div className="two">
+    <PopupButton
+      id="pmedcZ7N"
+      style={buttonStyle}
+      size={66}
+    >
+      <span style={{ marginLeft: 10 }}>🌱 Give Your Valuable Feedback</span>
+    </PopupButton>
+  </div>
+  <Box opacity={0.8} fontSize="2xl" textAlign={{ base: "center", md: "initial" }} className="one" ms={{ md: "150px", lg: "150px" }}>
+    🎯 Built by Team{" "}
+    <Link color="teal.500" href="https://github.com/Utkarshn10/Focusly">
+      Focusly
+    </Link>
+  </Box>
+</Grid>
+
     </Container>
   );
 };
