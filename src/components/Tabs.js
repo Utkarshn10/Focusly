7698b729570bf23817d4ca617f898c8404097de1
lokeshvasnik <lokeshvasnik2003@gmail.com>
2023-08-12 19:16:24 +0530

    fix(app): Tabs fixes #99 Fixed the Navlink buttons
    Closes: Tabs fixes #99

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -4,27 +4,27 @@ import { NavLink } from "react-router-dom";
 import { Tabs, Tab, TabList, Box, Center } from "@chakra-ui/react";
 
 function TabPanel() {
-  const theme = useTheme();
+    const theme = useTheme();
 
-  return (
-    <Center>
-      <Box py={4}>
-        <Tabs variant="soft-rounded" colorScheme="teal">
-          <TabList>
-            <Tab borderRadius={theme.radii.lg}>
-              <NavLink to="/">Work</NavLink>
-            </Tab>
-            <Tab borderRadius={theme.radii.lg}>
-              <NavLink to="/Lofi">Lo-fi</NavLink>
-            </Tab>
-            <Tab borderRadius={theme.radii.lg}>
-              <NavLink to="/Meditate">Meditate</NavLink>
-            </Tab>
-          </TabList>
-        </Tabs>
-      </Box>
-    </Center>
-  );
+    return (
+        <Center>
+            <Box py={4}>
+                <Tabs variant="soft-rounded" colorScheme="teal">
+                    <TabList>
+                        <NavLink to="/">
+                            <Tab borderRadius={theme.radii.lg}>Work</Tab>
+                        </NavLink>
+                        <NavLink to="/Lofi">
+                            <Tab borderRadius={theme.radii.lg}>Lo-fi</Tab>
+                        </NavLink>
+                        <NavLink to="/Meditate">
+                            <Tab borderRadius={theme.radii.lg}>Meditate</Tab>
+                        </NavLink>
+                    </TabList>
+                </Tabs>
+            </Box>
+        </Center>
+    );
 }
 
 export default TabPanel;
